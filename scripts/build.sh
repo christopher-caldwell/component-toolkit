4ed7a44446b6d7e08878ff61b9539a75ac1e0a8a
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2021-10-14 15:14:20 -0400

    feat: Adding build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-microbundle \
-  --jsx 'React.createElement' \
-  --jsxImportSource react \
-  --globals react/jsx-runtime=jsx \
-  --format modern,cjs
+tsc
+
+tscpaths -p tsconfig.json -s src -o dist > /dev/null
+
+# microbundle \
+#   --jsx 'React.createElement' \
+#   --jsxImportSource react \
+#   --globals react/jsx-runtime=jsx \
+#   --format modern,cjs
